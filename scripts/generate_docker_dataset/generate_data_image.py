da628b4a03778020fbedd24fe4c5c6938023df10
Paul Annetts <paul.annetts@hutoma.com>
2018-10-12 21:56:13 +0100

    Fix generate data image script

--- scripts/generate_docker_dataset/generate_data_image.py
+++ scripts/generate_docker_dataset/generate_data_image.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 from pathlib import Path
 import hu_build.build_docker
 
@@ -16,10 +17,12 @@ SCRIPT_PATH = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
 def main(args):
-    file_name = args.input_file
+    file_path = Path(args.input_file)
     out_path = SCRIPT_PATH / "out"
-
-    docker_line = "COPY {} /data/word2vec.v2.data.pkl".format(file_name, file_name)
+    out_path.mkdir(exist_ok=True)
+    print("Copying file {} to out/ directory".format(file_path))
+    shutil.copy2(file_path, out_path)
+    docker_line = "COPY {} /data/word2vec.v2.data.pkl".format(file_path.name)
     docker_file_content = DOCKER_TEMPLATE.format(to_be_replaced=docker_line)
     dockerfile = out_path / "Dockerfile"
     dockerfile.write_text(docker_file_content, encoding="utf8")
@@ -37,6 +40,7 @@ if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(
         description='Build dockerised image for an image')
     PARSER.add_argument('input_file', help='Input name of word2vec data PKL file')
+    PARSER.add_argument('word2vec_variant', help='Variant name of word2vec, usually the language')
     PARSER.add_argument('--docker-tag', help='Docker tag', default='1.0')
     PARSER.add_argument(
         '--docker-push', help='Push docker images to GCR', action="store_true")
